515bf234fd9a483b8ff16cb276cfdb52a67a6da9
AlbertCollante <ackm.1124@gmail.com>
2023-11-30 15:30:53 -0500

    Primer dia

---
+++ proyects/holaMundo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+
+export function App (){
+    return (
+        <>
+            < userName="midudev" name="Miguel Angel Durant"/>
+            <TwitterFollowCard userName="midudev" name="Miguel Angel Durant"></TwitterFollowCard>
+        </>
+        
+    )
+}
